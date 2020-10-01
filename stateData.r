8f5bf453996dd64329165858ef9c39d3881a29bf
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-01 13:33:52 -0400

    early version of state .r file

--- stateData.r
+++ stateData.r
@@ -1,3 +1,11 @@
+#imports
+library(dbplyr)
+
 # this file takes in state data
 
-# more code goes here
+# .csv file with our data is imported
+stateData <- read.csv("Table 21.csv", header = TRUE)
+
+# we delete rows that are not state names (United States, Regions)
+# rows to be deleted: 1, 2, 9, 16, 30, 43, 48, 54, 61, 62, 63
+stateData <- stateData[-c(1, 2, 9, 16, 30, 43, 48, 54, 61, 62, 63), ]
