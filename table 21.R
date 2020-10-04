3e7c2ef4fc7e99d8f908badd15129d747a861801
Grace Sullivan <sullivang21@mail.wlu.edu>
2020-10-03 21:58:24 -0400

    gather

--- table 21.R
+++ table 21.R
@@ -4,6 +4,8 @@ library(readr)
 
 table21 <- read_csv("Table 21.csv")
 
-tidy21 <- table21 %>%
-  gather()
+tidy21 <- gather(table21[1:25],
+                 key = "year",
+                 value = "USD",
+                 2:25)
   
