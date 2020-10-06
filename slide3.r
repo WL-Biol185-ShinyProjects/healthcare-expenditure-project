32e013f271b453b146478c09f6b80d97de5ee908
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-06 13:39:13 -0400

    line changes

--- slide3.r
+++ slide3.r
@@ -5,9 +5,8 @@ library(readr)
 library(ggplot2)
 
 slide3_data <- read_csv("Table 11 HE by state of residence per capita.csv")
-
-
 names(slide3_data) <- lapply(slide3_data[1, ], as.character)
+
 slide3 <- slide3_data[-1,]
 
 tidy_slide3 <- gather(slide3[1:25],
