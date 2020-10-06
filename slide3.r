99ae43318b51d0684a5ce29a1fa5b8807bdce138
Grace Sullivan <sullivang21@mail.wlu.edu>
2020-10-06 14:34:38 -0400

    ggplot

--- slide3.r
+++ slide3.r
@@ -7,6 +7,9 @@ library(ggplot2)
 slide3_data <- read_csv("Table 11 HE by state of residence per capita.csv")
 names(slide3_data) <- lapply(slide3_data[1, ], as.character)
 
+# slide3_del <- slide3_data[-c(62), ]
+# slide3_del <- slide3_data[-c(2, 9, 16, 30, 43, 48, 54, 61, 62), ]
+
 slide3 <- slide3_data[-1,]
 
 tidy_slide3 <- gather(slide3[1:25],
@@ -14,18 +17,24 @@ tidy_slide3 <- gather(slide3[1:25],
                       value = "USD",
                       2:25) %>%
   rename(State = "Region/state of residence") %>%
+  filter(!is.na(USD)) %>%
   mutate(Year = as.numeric(year)) %>%
   select(-(year))
 
-
 tidy_slide3$USD <- gsub("$", "", tidy_slide3$USD, fixed = TRUE)
+tidy_slide3$USD <- gsub(",", "", tidy_slide3$USD, fixed = TRUE)
 
-tidy_slide3 %>% mutate(dollars = as.numeric(USD))
 
-NE_year <- tidy_slide3$Year[tidy_slide3$State == "New England"]
-NE_dollars <- tidy_slide3$USD[tidy_slide3$State == "New England"]
-NE_dollars <- as.numeric(NE_dollars)
+tidy_slide3 <- tidy_slide3 %>%
+  mutate(dollars = as.numeric(USD)) %>%
+  select(-(USD))
 
-ggplot(tidy_slide3, aes(NE_year, NE_dollars)) +
+US <- tidy_slide3 %>%
+  filter(State == "United States")
+
+ggplot(US, aes(Year, dollars)) +
   geom_line()
+
+regions <- tidy_slide3 %>%
+  filter(State == "New England" | State == "Mideast" | State == "Great Lakes") | State == "Plains" | State == "Southeast" | State == "Southwest" | State == "Rocky Mountains" | State == "Far West")
 
