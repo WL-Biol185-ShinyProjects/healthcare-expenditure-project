139062d57f653f159efda3ac0152613e605c2ad9
Grace Sullivan <sullivang21@mail.wlu.edu>
2020-10-06 13:39:48 -0400

    hi

--- slide3.r
+++ slide3.r
@@ -18,9 +18,10 @@ tidy_slide3 <- gather(slide3[1:25],
   mutate(Year = as.numeric(year)) %>%
   select(-(year))
 
-US <- tidy_slide3$year[tidy_slide3$state == "United States"]
 
 tidy_slide3$USD <- gsub("$", "", tidy_slide3$USD, fixed = TRUE)
+
+tidy_slide3 %>% mutate(dollars = as.numeric(USD))
 
 NE_year <- tidy_slide3$Year[tidy_slide3$State == "New England"]
 NE_dollars <- tidy_slide3$USD[tidy_slide3$State == "New England"]
