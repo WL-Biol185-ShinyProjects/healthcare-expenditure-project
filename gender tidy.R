10840cc5c7af4ef5ff05acfdf5c18cf75d54ddb8
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-06 16:39:10 -0400

    Second push on the gender tidy.r file

--- gender tidy.R
+++ gender tidy.R
@@ -1,19 +1,29 @@
+#import statements
 library(tidyverse)
 library(tidyr)
 library(dplyr)
 library(readr)
 library(ggplot2)
 
-table7_0 <- read_csv("Table 7 personal health care per capita AGE GENDER.csv")
-names(table7_0) <- lapply(table7_0[3, ], as.character)
-table7 <- table7_0[-2,]
+slide4_data <- read_csv("Table 7 personal health care per capita AGE GENDER.csv")
+names(slide4_data) <- lapply(slide4_data[3, ], as.character)
+slide4 <- slide4_data[-2,]
 
-# names(table7_new) <- lapply(table7[1, ], as.character)
-# table7_new <- table7_new[-1,]
+names(table7_new) <- lapply(table7[1, ], as.character)
+table7_new <- table7_new[-1,]
 
-table7_tidy <- gather(table7[1:8], 
-                      key = "Year",
+#just want the levels, not the average annual growth
+#delete the average annual growth
+
+#the gather function should only take the first 8
+tidy_slide4 <- gather(slide4[1:8],
+                      key = "year",
                       value = "USD",
-                      2:8
-                      ) %>%
-  filter(!is.na(USD) | )
+                      2:8) %>%
+  rename(State = "Region/state of residence") %>%
+  filter(!is.na(USD)) %>%
+  mutate(Year = as.numeric(year)) %>%
+  select(-(year))
+
+
+
