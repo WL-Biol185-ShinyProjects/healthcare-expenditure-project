7a9d28e0e43614b9e408ddac77f569df21d68767
Grace Sullivan <sullivang21@mail.wlu.edu>
2020-10-06 13:00:44 -0400

    table formatting

--- slide3.r
+++ slide3.r
@@ -1,9 +1,17 @@
 library(tidyverse)
 library(tidyr)
+library(dplyr)
 library(readr)
 
 slide2_data <- read_csv("Table 11 HE by state of residence per capita.csv")
 
 
 names(slide2_data) <- lapply(slide2_data[1, ], as.character)
-slide2_data <- slide2_data[-1,]
+slide2 <- slide2_data[-1,]
+
+slide_new <- slide2_data %>%
+  rownames_to_column("1") %>%
+  rename(State = "Region/state of residence")
+  
+
+slide_new <- subset(!slide_new["Region/state of residence"] == "United States")
