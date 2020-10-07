caf0d532351cafaf3faa662b139eae0d88f68b3c
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-06 22:35:18 -0400

    working graph, need to add other columns in graphs

--- source_Of_Funds_Slide.r
+++ source_Of_Funds_Slide.r
@@ -15,20 +15,52 @@ slide5_data <- slide5_data[-1:-3,]
 #removes the 3 empty columns
 slide5_data <- slide5_data[,-10:-12]
 
+#remove rows at the end
+slide5_data <- slide5_data[-23:-113,]
+
 #rename all of the column headers to be one word
+slide5_data <- rename(slide5_data, total = "Total1")
 slide5_data <- rename(slide5_data, outOfPocket = "Out of Pocket")
 slide5_data <- rename(slide5_data, healthInsurance = "Health Insurance2")
 slide5_data <- rename(slide5_data, privateHealthInsurance = "Private Health Insurance")
-
-
+slide5_data <- rename(slide5_data, medicare = "Medicare")
+slide5_data <- rename(slide5_data, medicaid = "Medicaid")
+slide5_data <- rename(slide5_data, otherHealthInsurancePrograms = "Other Health Insurance Programs3")
+slide5_data <- rename(slide5_data, otherThirdPartyPayers = "Other Third Party Payers4")
 
 #removes all of the "$"
-#slide5_data$Total1 <- gsub("$", "", slide5_data$Total1, fixed = TRUE)
-#slide5_data$OutofPocket <- gsub("$", "", slide5_data$OutofPocket, fixed = TRUE)
+slide5_data$total <- gsub("$", "", slide5_data$total, fixed = TRUE)
+slide5_data$outOfPocket <- gsub("$", "", slide5_data$outOfPocket, fixed = TRUE)
+slide5_data$healthInsurance <- gsub("$", "", slide5_data$healthInsurance, fixed = TRUE)
+slide5_data$privateHealthInsurance <- gsub("$", "", slide5_data$privateHealthInsurance, fixed = TRUE)
+slide5_data$medicare <- gsub("$", "", slide5_data$medicare, fixed = TRUE)
+slide5_data$medicaid <- gsub("$", "", slide5_data$medicaid, fixed = TRUE)
+slide5_data$otherHealthInsurancePrograms <- gsub("$", "", slide5_data$otherHealthInsurancePrograms, fixed = TRUE)
+slide5_data$otherThirdPartyPayers <- gsub("$", "", slide5_data$otherThirdPartyPayers, fixed = TRUE)
 
 #removes all of the ","
-#slide5_data$Total1 <- gsub(",", "", slide5_data$Total1, fixed = TRUE)
+slide5_data$total <- gsub(",", "", slide5_data$total, fixed = TRUE)
+slide5_data$outOfPocket <- gsub(",", "", slide5_data$outOfPocket, fixed = TRUE)
+slide5_data$healthInsurance <- gsub(",", "", slide5_data$healthInsurance, fixed = TRUE)
+slide5_data$privateHealthInsurance <- gsub(",", "", slide5_data$privateHealthInsurance, fixed = TRUE)
+slide5_data$medicare <- gsub(",", "", slide5_data$medicare, fixed = TRUE)
+slide5_data$medicaid <- gsub(",", "", slide5_data$medicaid, fixed = TRUE)
+slide5_data$otherHealthInsurancePrograms <- gsub(",", "", slide5_data$otherHealthInsurancePrograms, fixed = TRUE)
+slide5_data$otherThirdPartyPayers <- gsub(",", "", slide5_data$otherThirdPartyPayers, fixed = TRUE)
 
+#make all of the columns numeric
+slide5_data <- mutate(slide5_data, total = as.numeric(total))
+slide5_data <- mutate(slide5_data, outOfPocket = as.numeric(outOfPocket))
+slide5_data <- mutate(slide5_data, healthInsurance = as.numeric(healthInsurance))
+slide5_data <- mutate(slide5_data, privateHealthInsurance = as.numeric(privateHealthInsurance))
+slide5_data <- mutate(slide5_data, medicare = as.numeric(medicare))
+slide5_data <- mutate(slide5_data, medicaid = as.numeric(medicaid))
+slide5_data <- mutate(slide5_data, otherHealthInsurancePrograms = as.numeric(otherHealthInsurancePrograms))
+slide5_data <- mutate(slide5_data, otherThirdPartyPayers = as.numeric(otherThirdPartyPayers))
+
+#graph
+ggplot(slide5_data, aes(Year, total)) +
+   geom_point()
 
 # tidy_slide5 <- gather(slide5[1:9])
 #                     key = "year",
