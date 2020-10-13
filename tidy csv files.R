914a0170377f2c477b4b5c6b3349364cee0da71d
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-13 13:15:35 -0400

    taking out a few files

--- tidy csv files.R
+++ tidy csv files.R
@@ -2,7 +2,9 @@ library(lubridate)
 library(tidyverse)
 
 #takes all of the files in our folder and keeps the full name of the files
-files <- list.files(path = "stateCSVFiles/", full.names = TRUE)
+files <- list.files(path = "stateCSVFiles", full.names = TRUE)
+
+head(files)
 
 tables <- lapply(c(files),
                  function(table) {
@@ -43,40 +45,37 @@ tables <- lapply(c(files),
 table1 <- tables[[1]]         
 
 
-tidyTables <- function(table) {
-  
-  # read in csv file
-  data <- read_csv(table)
-  
-  # replacing column x names with years, removing duplicate column header row
-  names(data) <- lapply(data[1, ], as.character)
-  data <- data[-1,]
-  
-  # gather years into a column
-  # rename column, remove NA values, convert year data type to numeric, remove original year column
-  tidyTable <- gather(data[1:25],
-                      key = "year",
-                      value = "USD",
-                      2:25) %>%
-    rename(State = "Region/state of residence") %>%
-    filter(!is.na(USD)) %>%
-    mutate(Year = as.numeric(year)) %>%
-    select(-(year))
-  
-  # remove "$" and "," from USD variable
-  tidyTable$USD <- gsub("$", "", tidyTable$USD, fixed = TRUE)
-  tidyTable$USD <- gsub(",", "", tidyTable$USD, fixed = TRUE)
+# tidyTables <- function(table) {
+#   
+#   # read in csv file
+#   data <- read_csv(table)
+#   
+#   # replacing column x names with years, removing duplicate column header row
+#   names(data) <- lapply(data[1, ], as.character)
+#   data <- data[-1,]
+#   
+#   # gather years into a column
+#   # rename column, remove NA values, convert year data type to numeric, remove original year column
+#   tidyTable <- gather(data[1:25],
+#                       key = "year",
+#                       value = "USD",
+#                       2:25) %>%
+#     rename(State = "Region/state of residence") %>%
+#     filter(!is.na(USD)) %>%
+#     mutate(Year = as.numeric(year)) %>%
+#     select(-(year))
+#   
+#   # remove "$" and "," from USD variable
+#   tidyTable$USD <- gsub("$", "", tidyTable$USD, fixed = TRUE)
+#   tidyTable$USD <- gsub(",", "", tidyTable$USD, fixed = TRUE)
+# 
+#   # convert USD data type to numeric
+#   tidyTable <- tidyTable %>%
+#     mutate(dollars = as.numeric(USD)) %>%
+#     select(-(USD))
+#   
+#   # return new table
+#   tidyTable
+#   
+# }
 
-  # convert USD data type to numeric
-  tidyTable <- tidyTable %>%
-    mutate(dollars = as.numeric(USD)) %>%
-    select(-(USD))
-  
-  # return new table
-  tidyTable
-  
-}
-
-
-table11 <- tidyTables("Table 11 HE by state of residence per capita.csv")
-table12 <- tidyTables("Table 12 Hospital expenditures by state per capita.csv")
