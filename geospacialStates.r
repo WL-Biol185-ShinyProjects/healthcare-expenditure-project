754dbeea86b6e29665997d02c2dae2657a00a6d2
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-20 13:13:18 -0400

    work in progress

--- geospacialStates.r
+++ geospacialStates.r
@@ -1,10 +1,13 @@
-library(ggplot2)
-library(dplyr)
-states<-as.data.frame(state.x77)
-states$region <- tolower(rownames(states))
-states_map <- map_data("state")
-fact_join$`Life Exp` <- as.numeric(fact_join$`Life Exp`)
-ggplot(fact_join, aes(long, lat, group = group))+
-  geom_polygon(aes(fill = `Life Exp`), color = "white")+
-  scale_fill_viridis_c(option = "C")+
-  theme_classic()
+library(geojson)
+
+states <- geojsonio::geojson_read("us-states.geojson", what = "sp")
+class(states)
+
+names(states)
+
+m <- leaflet(states) %>%
+  setView(-96, 37.8, 4) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
+
