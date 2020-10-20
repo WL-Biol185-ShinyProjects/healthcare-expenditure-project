d71c582cdaa050c9ae168b64de415479fc8c400a
sullivang21 <sullivang21@mail.wlu.edu>
2020-10-20 12:44:47 -0400

    server and ui scripts for interactive plots

--- server.R
+++ server.R
@@ -1,17 +1,39 @@
 library(shiny)
+library(tidyverse)
+library(nycflights13)
 
 # Define server logic required to draw a histogram
 function(input, output) {
   
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    output$distancePlot <- renderPlot({
+      flights %>%
+        filter(carrier %in% input$airline) %>%
+        ggplot(aes(distance, air_time)) +
+          geom_point()
+      
+    })
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$flightInfo <- renderDataTable({
+      clickEvent <- input$distancePlotClick
+      flights %>%
+        filter(carrier %in% input$airline) %>%
+        nearPoints(clickEvent)
+      
+    })
     
   })
   
 }
+    
+    # # generate bins based on input$bins from ui.R
+    # x    <- faithful[, 2] 
+    # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # 
+    # # draw the histogram with the specified number of bins
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+#   })
+#   
+# }
--- ui.R
+++ ui.R
@@ -1,21 +1,43 @@
 library(shiny)
+# library(leaflet)
+library(nycflights13)
 
-
-source("sidebar.R")
-source("main-panel.R")
+# 
+# source("sidebar.R")
+# source("main-panel.R")
 
 # Define UI for application that draws a histogram
 fluidPage(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(sidebar, main_panel),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+  sidebarPanel(
+    selectInput(inputId = "airline",
+                label = "Select airlines:",
+                choices = unique(flights$carrier),
+                multiple = TRUE,
+                selected = "UA"
+              )
+  ),
+  mainPanel(
+    plotOutput("distancePlot",
+               click = "distancePlotClick"
+              ),
+    tableOutput("flightInfo")
   )
+)
+  
+  
+  
+  
+  
+  # # Application title
+  # titlePanel("Old Faithful Geyser Data"),
+  # 
+  # # Sidebar with a slider input for number of bins 
+  # sidebarLayout(sidebar, main_panel),
+  #   
+  #   # Show a plot of the generated distribution
+  #   mainPanel(
+  #     plotOutput("distPlot")
+  #   )
+  # )
 
