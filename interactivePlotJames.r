03deb7b2530269df304afb94a033bddc2849d088
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-20 12:32:52 -0400

    playing with interactive plots

---
+++ interactivePlotJames.r
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(shiny)
+library(nycflights13)
+
+function(input, output) {
+  
+  output$distancePlot <- renderPlot({
+    flights %>%
+      filter(carrier == input$airline) %>%
+      ggplot(aes(distance, air_time)) + 
+      geom_point()
+  })
+  
+  output$flightInfo <- renderText({
+    "This is just text"
+  })
+}
