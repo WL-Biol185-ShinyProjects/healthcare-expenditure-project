20b4625fc7b9901faab9e1d2bce8906b41c37c93
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-20 13:36:51 -0400

    progress

--- geospacialStates.r
+++ geospacialStates.r
@@ -1,13 +1,36 @@
-library(geojson)
+library(tidyverse)
+library(leaflet)
+library(rgdal)
+leafletdf <- read_csv("leafletdf.csv")
+state_names<- read_csv("states.csv")
+statesGeo  <- rgdal::readOGR("states.geo.json")
 
-states <- geojsonio::geojson_read("us-states.geojson", what = "sp")
-class(states)
+#merge data frame into states
+statesGEO@data <- statesGEO@data %>%
+  left_join(state_names, by = c("NAME" = "State")) %>%
+  left_join(leafletdf, by = c("Abbreviation" = "state"))
 
-names(states)
-
-m <- leaflet(states) %>%
-  setView(-96, 37.8, 4) %>%
-  addProviderTiles("MapBox", options = providerTileOptions(
-    id = "mapbox.light",
-    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
-
+pal <- colorNumeric("YlOrRd", NULL)
+map<-
+  leaflet(data= statesGEO) %>%
+  setView(-96, 37.8, 4)%>%
+  addTiles() %>%
+  addPolygons(stroke = FALSE,
+              smoothFactor     = 0.3,
+              fillOpacity      = 0.7,
+              opacity          = 1,
+              dashArray        = "3",
+              weight           = 2,
+              color            = "white",
+              fillColor        = ~pal(leafletdf$stateAvg),
+              label            = ~paste0(NAME, ": ", formatC(leafletdf$stateAvg)),
+              highlightOptions = highlightOptions(color = "white",
+                                                  fillOpacity = 2,
+                                                  bringToFront
+  addLegend("bottomright", = TRUE
+              )) %>%
+            pal          = pal,
+            values       = ~(leafletdf$stateAvg),
+            opacity      = 0.8,
+            title        = "Does this work",
+            labFormat    = labelFormat(suffix = "%"))
