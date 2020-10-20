18bd1128f4e8184a30f2a557498f422ccd67dd87
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-20 12:56:17 -0400

    playing around with geospacial

--- geospacialStates.r
+++ geospacialStates.r
@@ -1,1 +1,10 @@
-download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip" , destfile="world_shape_file.zip")
+library(ggplot2)
+library(dplyr)
+states<-as.data.frame(state.x77)
+states$region <- tolower(rownames(states))
+states_map <- map_data("state")
+fact_join$`Life Exp` <- as.numeric(fact_join$`Life Exp`)
+ggplot(fact_join, aes(long, lat, group = group))+
+  geom_polygon(aes(fill = `Life Exp`), color = "white")+
+  scale_fill_viridis_c(option = "C")+
+  theme_classic()
