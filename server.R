3d0c3ee48debe12072254e433a89bc67981cb5b3
sullivang21 <sullivang21@mail.wlu.edu>
2020-10-21 14:51:15 -0400

    more server work

--- server.R
+++ server.R
@@ -8,12 +8,13 @@ function(input, output) {
   output$expenditurePlot <- renderPlot({
     expend <- input$expenditure
     df <- tables$expend
-    # state_df <- df %>%
-    #   select(State == input$state)
-    # ggplot(state_df, aes(Year, dollars)) +
-    #   geom_line()
-    ggplot(df, aes(df$Year[df$State == input$state], df$dollars[df$State == input$state])) +
+    state <- toString(input$state)
+    state_df <- df %>%
+      filter(State == state)
+    ggplot(state_df, aes(Year, dollars)) +
       geom_line()
+    # ggplot(df, aes(df$Year[df$State == input$state], df$dollars[df$State == input$state])) +
+    #   geom_line()
     
   })
     
