052a54a99f00edc1cb07561ee67318ea67846ce6
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-22 13:19:29 -0400

    cleaned up ui.r file

--- ui.R
+++ ui.R
@@ -6,9 +6,9 @@ library(shinydashboard)
 # source("main-panel.R")
 
 # Define UI for application
-dashboardPage(
-  dashboardHeader(title = "HeathcareExpenditures"),
-  dashboardSidebar(
+dashboardPage(skin = "green",
+  dashboardHeader(title = "Heathcare Expenditures in the United States", titleWidth = 600),
+  dashboardSidebar(width = 240,
     sidebarMenu(
       menuItem("States", tabName = "statesTab", icon = icon("map-marker-alt")),
       menuItem("Gender", tabName = "genderTab", icon = icon("user-friends")),
@@ -47,49 +47,4 @@ dashboardPage(
     
   )
 )
-  
-  # dashboardPage(skin = "green",
-  #               dashboardHeader(title = "Healthcare Expenditures in the United States", titleWidth = 600),
-  #               dashboardSidebar(width = 120,
-  #                 sidebarMenu(
-  #                   # menuItem("Gender", tabName = "genderTab"),
-  #                   # menuItem("Age", tabName = "ageTab"),
-  #                   menuItem("States", tabName = "statesTab")
-  #                 )
-  #               ),
-  #               
-  # dashboardBody(
-  #   
-  #   
-  #   
-  #   tabItems(
-  #     # 
-  #     # tabItem(tabName = "ag
-  
-  
-  #titlePanel("Healthcare Expenditures by State"),
-  
-#   sidebarPanel(
-#     selectInput(inputId = "expenditure",
-#                 label = "Select expenditures:",
-#                 choices = names(tables),
-#                 multiple = TRUE,
-#                 selected = "Population"
-#               ),
-#     selectInput(inputId = "state",
-#                 label = "Select states:",
-#                 choices = unique(tables$Population$State),
-#                 multiple = TRUE,
-#                 selected = "Virginia")
-#   ),
-#   mainPanel(
-#     plotOutput(outputId = "expenditurePlot"
-#                # click = "expenditurePlotClick"
-#               ),
-#     leafletOutput(outputId = "leafletPlot"),
-#     tableOutput("expenditureInfo")
-#     
-#   )
-# )
-# )
 
