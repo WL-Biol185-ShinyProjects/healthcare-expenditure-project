2b394c3c298a01fa29883c6d670595067bee8409
Yamase31 <lawsonj21@mail.wlu.edu>
2020-10-22 12:55:30 -0400

    UI.r updated

--- ui.R
+++ ui.R
@@ -1,54 +1,70 @@
 library(shiny)
 library(leaflet)
+library(shinydashboard)
  
 # source("sidebar.R")
 # source("main-panel.R")
 
 # Define UI for application
-fluidPage(
-  
-  dashboardPage(skin = "black",
-                dashboardHeader(title = "Healthcare Expenditures in the United States"),
-                dashboardSidebar(
-                  sidebarMenu(
-                    # menuItem("Gender", tabName = "genderTab"),
-                    # menuItem("Age", tabName = "ageTab"),
-                    menuItem("States", tabName = "statesTab")
-                  )
-                ),
-                
+dashboardPage(
+  dashboardHeader(title = "HeathcareExpenditures"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("States", tabName = "statesTab", icon = icon("th"))
+    )
+  ),
   dashboardBody(
     tabItems(
-      # tabItem(tabName = "genderTab", genderTab),
-      # tabItem(tabName = "ageTab", ageTab),
-      tabItem(tabName = "statesTab", statesTab)
+      tabItem(tabName = "genderTab", fluidRow(
+        
+      ))
     )
-  )
-  
-  
-  titlePanel("Healthcare Expenditures by State"),
-  
-  sidebarPanel(
-    selectInput(inputId = "expenditure",
-                label = "Select expenditures:",
-                choices = names(tables),
-                multiple = TRUE,
-                selected = "Population"
-              ),
-    selectInput(inputId = "state",
-                label = "Select states:",
-                choices = unique(tables$Population$State),
-                multiple = TRUE,
-                selected = "Virginia")
-  ),
-  mainPanel(
-    plotOutput(outputId = "expenditurePlot"
-               # click = "expenditurePlotClick"
-              ),
-    leafletOutput(outputId = "leafletPlot"),
-    tableOutput("expenditureInfo")
     
   )
 )
-)
+  
+  # dashboardPage(skin = "green",
+  #               dashboardHeader(title = "Healthcare Expenditures in the United States", titleWidth = 600),
+  #               dashboardSidebar(width = 120,
+  #                 sidebarMenu(
+  #                   # menuItem("Gender", tabName = "genderTab"),
+  #                   # menuItem("Age", tabName = "ageTab"),
+  #                   menuItem("States", tabName = "statesTab")
+  #                 )
+  #               ),
+  #               
+  # dashboardBody(
+  #   
+  #   
+  #   
+  #   tabItems(
+  #     # 
+  #     # tabItem(tabName = "ag
+  
+  
+  #titlePanel("Healthcare Expenditures by State"),
+  
+#   sidebarPanel(
+#     selectInput(inputId = "expenditure",
+#                 label = "Select expenditures:",
+#                 choices = names(tables),
+#                 multiple = TRUE,
+#                 selected = "Population"
+#               ),
+#     selectInput(inputId = "state",
+#                 label = "Select states:",
+#                 choices = unique(tables$Population$State),
+#                 multiple = TRUE,
+#                 selected = "Virginia")
+#   ),
+#   mainPanel(
+#     plotOutput(outputId = "expenditurePlot"
+#                # click = "expenditurePlotClick"
+#               ),
+#     leafletOutput(outputId = "leafletPlot"),
+#     tableOutput("expenditureInfo")
+#     
+#   )
+# )
+# )
 
