0dbfb28a17ca49dd7b8b60f7c4017ebbd6b2d2ad
sullivang21 <sullivang21@mail.wlu.edu>
2020-10-22 12:25:49 -0400

    new ui

--- ui.R
+++ ui.R
@@ -11,16 +11,16 @@ fluidPage(
                 dashboardHeader(title = "Healthcare Expenditures in the United States"),
                 dashboardSidebar(
                   sidebarMenu(
-                    menuItem("Gender", tabName = "genderTab"),
-                    menuItem("Age", tabName = "ageTab"),
+                    # menuItem("Gender", tabName = "genderTab"),
+                    # menuItem("Age", tabName = "ageTab"),
                     menuItem("States", tabName = "statesTab")
                   )
                 ),
                 
   dashboardBody(
     tabItems(
-      tabItem(tabName = "genderTab", genderTab),
-      tabItem(tabName = "ageTab", ageTab),
+      # tabItem(tabName = "genderTab", genderTab),
+      # tabItem(tabName = "ageTab", ageTab),
       tabItem(tabName = "statesTab", statesTab)
     )
   )
