7690a2da9ffb5e191d6c47f481b922647073f058
sullivang21 <sullivang21@mail.wlu.edu>
2020-10-27 12:32:24 -0400

    more gender/age tables and updates to gender tidy

--- gender tidy.R
+++ gender tidy.R
@@ -7,8 +7,9 @@ library(ggplot2)
 
 
 files_age <- list.files(path = "AgeGender1", full.names = TRUE)
+files_age_2 <- list.files(path = "AgeGender2", full.names = TRUE)
 
-tables_age <- lapply(c(files_age),
+tables_age_2 <- lapply(c(files_age_2),
                      function(table) {
                        
                        # read in csv file
@@ -37,6 +38,50 @@ tables_age <- lapply(c(files_age),
                          select(-(year))
                        
                        
+                       male_female <- data[-1:-8,]
+                       
+                       male <- male_female[-9:-17,] 
+                       
+                       male_2 <- gather(male[1:8],
+                                        key = "year",
+                                        value = "USD",
+                                        2:8) %>%
+                         mutate(group = "male") %>%
+                         rename(ageGroup = "Age group")
+                       
+                       male_2$USD <- gsub(",", "", male_2$USD, fixed = TRUE)
+                       
+                       male_2 <- male_2 %>%
+                         mutate(year = as.numeric(year),
+                                dollars = as.numeric(USD)) %>%
+                         select(ageGroup, group, year, dollars) %>%
+                         rename(USD = dollars)
+                       
+                       female <- male_female[-1:-8,]
+                       
+                       female_2 <- gather(female[1:8],
+                                          key = "year",
+                                          value = "USD",
+                                          2:8) %>%
+                         mutate(group = "female") %>%
+                         rename(ageGroup = "Age group") %>%
+                         filter(!is.na(USD))
+                       
+                       female_2$USD <- gsub(",", "", female_2$USD, fixed = TRUE)
+                       
+                       female_2 <- female_2 %>%
+                         mutate(year = as.numeric(year),
+                                dollars = as.numeric(USD)) %>%
+                         select(ageGroup, group, year, dollars) %>%
+                         rename(USD = dollars)
+                       
+                       
+                       gender <- bind_rows(total_2, male_2, female_2)
+                       
+                       gender_total <- gender %>%
+                         filter(ageGroup == "Total" | ageGroup == "Males" | ageGroup == "Females")
+                       
+                       
                      }
 )
 
@@ -48,8 +93,47 @@ names(tables_age) <- c("TotalPersonalHealthCare",
                        "OutOfPocketSpending",
                        "OtherPayersAndProgramsSpending"
 )
+
+names(tables_age_2) <- c("OutOfPocketSpendingPerCapita",
+                         "OtherPayersAndProgramsSpendingPerCapita",
+                         "PersonalHealthCarePerCapita")
                    
-                   
+
+
+
+slide4_data <- read_csv("Table 7 personal health care per capita AGE GENDER.csv")
+
+# this line is setting the names of columns equal to the data in the third row which contains the years
+names(slide4_data) <- lapply(slide4_data[3, ], as.character)
+
+# this line removes rows 1:3
+slide4 <- slide4_data[-1:-3,]
+
+# removes unwanted client
+slide4 <- slide4[1:8]
+
+
+#the gather function should only take the first 8
+tidy_slide4 <- gather(slide4[1:8],
+                      key = "year",
+                      value = "USD",
+                      2:8) %>%
+  rename(ageGroup = "Age group") %>%
+  #filters out rows with "NA"
+  filter(!is.na(USD)) %>%
+  #converts years to numeric
+  mutate(Year = as.numeric(year))
+
+#removes all of the commas and replaces it with a "" character
+tidy_slide4$USD <- gsub(",", "", tidy_slide4$USD, fixed = TRUE)
+
+#converts string USD into int USD
+tidy_slide4 <- tidy_slide4 %>%
+  mutate(USD = as.numeric(USD)) %>%
+  select(-(year))
+
+####
+
 total <- slide4[-9:-25,]
 
 total_2 <- gather(total[1:8],
@@ -131,36 +215,6 @@ ggplot(age_females, aes(year, USD, color = ageGroup)) +
 
 
 
-# 
-# slide4_data <- read_csv("Table 7 personal health care per capita AGE GENDER.csv")
-# 
-# # this line is setting the names of columns equal to the data in the third row which contains the years
-# names(slide4_data) <- lapply(slide4_data[3, ], as.character)
-# 
-# # this line removes rows 1:3
-# slide4 <- slide4_data[-1:-3,]
-# 
-# # removes unwanted client
-# slide4 <- slide4[1:8]
-# 
-# 
-# #the gather function should only take the first 8
-# tidy_slide4 <- gather(slide4[1:8],
-#                       key = "year",
-#                       value = "USD",
-#                       2:8) %>%
-#   rename(ageGroup = "Age group") %>%
-#   #filters out rows with "NA"
-#   filter(!is.na(USD)) %>%
-#   #converts years to numeric
-#   mutate(Year = as.numeric(year))
-# 
-# #removes all of the commas and replaces it with a "" character
-# tidy_slide4$USD <- gsub(",", "", tidy_slide4$USD, fixed = TRUE)
-# 
-# #converts string USD into int USD
-# tidy_slide4 <- tidy_slide4 %>%
-#   mutate(USD = as.numeric(USD)) %>%
-#   select(-(year))
+
 #   
 
