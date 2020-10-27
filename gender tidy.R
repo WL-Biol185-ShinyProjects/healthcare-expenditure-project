23eabe7f725a5a13eadd92efaa3b2d5aa6304724
sullivang21 <sullivang21@mail.wlu.edu>
2020-10-27 12:48:26 -0400

    gender tidy and updates to server for plotting (wrong tab)

--- gender tidy.R
+++ gender tidy.R
@@ -10,6 +10,82 @@ files_age <- list.files(path = "AgeGender1", full.names = TRUE)
 files_age_2 <- list.files(path = "AgeGender2", full.names = TRUE)
 
 tables_age_2 <- lapply(c(files_age_2),
+                       function(table) {
+                         
+                         # read in csv file
+                         data <- read_csv(table)
+                         
+                         # setting proper column names and removing unnecessary rows
+                         names(data) <- lapply(data[3, ], as.character)
+                         data <- data[-1:-3,]
+                         data <- data[1:8]
+                         
+                         # gathering data to ensure tidy form, converting data types of year from string to numeric
+                         tidyTable <- gather(data[1:8],
+                                             key = "year",
+                                             value = "USD",
+                                             2:8) %>%
+                           rename(ageGroup = "Age group") %>%
+                           filter(!is.na(USD)) %>%
+                           mutate(Year = as.numeric(year))
+                         
+                         # removing all commas in data table
+                         tidyTable$USD <- gsub(",", "", tidyTable$USD, fixed = TRUE)
+                         
+                         # converts data type from string to numeric for USD column
+                         tidyTable <- tidyTable %>%
+                           mutate(USD = as.numeric(USD)) %>%
+                           select(-(year))
+                         
+                         
+                         male_female <- data[-1:-8,]
+                         
+                         male <- male_female[-9:-17,] 
+                         
+                         male_2 <- gather(male[1:8],
+                                          key = "year",
+                                          value = "USD",
+                                          2:8) %>%
+                           mutate(group = "male") %>%
+                           rename(ageGroup = "Age group")
+                         
+                         male_2$USD <- gsub(",", "", male_2$USD, fixed = TRUE)
+                         
+                         male_2 <- male_2 %>%
+                           mutate(year = as.numeric(year),
+                                  dollars = as.numeric(USD)) %>%
+                           select(ageGroup, group, year, dollars) %>%
+                           rename(USD = dollars)
+                         
+                         female <- male_female[-1:-8,]
+                         
+                         female_2 <- gather(female[1:8],
+                                            key = "year",
+                                            value = "USD",
+                                            2:8) %>%
+                           mutate(group = "female") %>%
+                           rename(ageGroup = "Age group") %>%
+                           filter(!is.na(USD))
+                         
+                         female_2$USD <- gsub(",", "", female_2$USD, fixed = TRUE)
+                         
+                         female_2 <- female_2 %>%
+                           mutate(year = as.numeric(year),
+                                  dollars = as.numeric(USD)) %>%
+                           select(ageGroup, group, year, dollars) %>%
+                           rename(USD = dollars)
+                         
+                         
+                         gender <- bind_rows(total_2, male_2, female_2)
+                         
+                         gender_total <- gender %>%
+                           filter(ageGroup == "Total" | ageGroup == "Males" | ageGroup == "Females")
+                         
+                         
+                       }
+)
+
+tables_age_2 <- lapply(c(files_age_2),
                      function(table) {
                        
                        # read in csv file
